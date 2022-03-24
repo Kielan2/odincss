a545fb0ba8a4feb29a42ead25b054dfefda171e3
Kielan2 <kielananderson@gmail.com>
2022-03-23 19:45:33 -0600

    started rock, paper, scissors

---
+++ odinrps/main.js
@@ -0,0 +1,9 @@
+document.getElementById('rock').addEventListener('click', computerPlay)
+document.getElementById('paper').addEventListener('click', computerPlay)
+document.getElementById('scissors').addEventListener('click', computerPlay)
+
+
+
+function computerPlay('rock', 'paper', 'scissors'){
+    if
+}
