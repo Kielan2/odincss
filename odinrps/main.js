48b80cf6f1a1f5a0c0e7f9b59417afa107f8d98c
Kielan2 <kielananderson@gmail.com>
2022-03-24 19:41:59 -0600

    rock paper scissors functions

--- odinrps/main.js
+++ odinrps/main.js
@@ -2,8 +2,33 @@ document.getElementById('rock').addEventListener('click', computerPlay)
 document.getElementById('paper').addEventListener('click', computerPlay)
 document.getElementById('scissors').addEventListener('click', computerPlay)
 
-
-
-function computerPlay(rock, paper, scissors){
-    iff
+function rps(){
+    let random = Math.random()
+    if (random < .33){
+        return 'rock'
+    } else if (random < .66){
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
 }
+
+
+function rpsTwo(playerChoice){
+    let botChoice = rps()
+    if ( (playerChoice === 'rock' && botChoice === 'scissors') ||
+    (playerChoice === 'paper' && botChoice === 'rock') || 
+    (playerChoice === 'scissors' && botChoice === 'paper') ){
+        return 'You Win'
+    } else if (playerChoice === botChoice){
+        return 'You Tie'
+    } else {
+        'You Lose'
+    }
+}
+
+function rpsThree(arr){
+    arr.forEach( choice => rpsTwo(choice) )
+}
+
+rpsThree(['rock', 'paper', 'scissors'])
